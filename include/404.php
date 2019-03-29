00935128c7b2c1dd8d07067435cef2917ccb351c
Kubsz <me@kubs.uk>
2019-03-29 16:23:09 +0000

    added .htaccess, improved and implemented 404 page

--- include/404.php
+++ include/404.php
@@ -1,1 +1,17 @@
-404 error
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <title>404 - Error</title>
+    <link rel="stylesheet" type="text/css" href="/css/home.css">
+    <?php include($_SERVER["DOCUMENT_ROOT"]."/include/head.php"); ?>
+</head>
+<body style="height:calc(100vh - 60px)">
+<?php include($_SERVER["DOCUMENT_ROOT"]."/include/nav.php"); ?>
+<div class="section section-404">
+    <div class="margined activity-feed">
+        <h1>404 Error</h1>
+        <a href="/">Return to home</a>
+    </div>
+</div>
+</body>
+</html>
